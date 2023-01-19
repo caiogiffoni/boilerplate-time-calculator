3a28ea66d18164de442c866e5656d89a0b3a52de
Caio Giffoni <95258181+caiogiffoni@users.noreply.github.com>
2023-01-19 21:16:42 +0000

    added readme and function with comment

--- time_calculator.py
+++ time_calculator.py
@@ -1,44 +1,46 @@
-Time Calculator
-You will be working on this project with our Replit starter code.
+import datetime
 
-Start by importing the project on Replit.
-Next, you will see a .replit window.
-Select Use run command and click the Done button.
-Write a function named add_time that takes in two required parameters and one optional parameter:
 
-a start time in the 12-hour clock format (ending in AM or PM)
-a duration time that indicates the number of hours and minutes
-(optional) a starting day of the week, case insensitive
-The function should add the duration time to the start time and return the result.
+def add_time(start, duration, weekDay=''):
+  print(start)
+  print(duration)
+  print(weekDay)
+  weekDay = weekDay.lower()
+  # daysPassed = math.floor(int(duration.split(':')[0]) / 24)
 
-If the result will be the next day, it should show (next day) after the time. If the result will be more than one day later, it should show (n days later) after the time, where "n" is the number of days later.
+  # print(daysPassed, ' daysPassed')
+  startTime = datetime.datetime.strptime(start, "%I:%M %p")
+  print(startTime, ' startTime')
 
-If the function is given the optional starting day of the week parameter, then the output should display the day of the week of the result. The day of the week in the output should appear after the time and before the number of days later.
+  # h = duration.split(':')[0]
+  # m = duration.split(':')[1]
+  # print(h, ' h')
+  # print(m, ' m')
 
-Below are some examples of different cases the function should handle. Pay close attention to the spacing and punctuation of the results.
+  # t = datetime.time(hour= int(duration.split(':')[0]), minute = int(duration.split(':')[1]))
+  # t = datetime.datetime.strptime(duration, "%H:%M")
+  durationDateime = datetime.timedelta(hours=int(duration.split(':')[0]),
+                                       minutes=int(duration.split(':')[1]))
+  print(durationDateime, ' durationDateime')
 
-add_time("3:00 PM", "3:10")
-# Returns: 6:10 PM
+  overTime = startTime + durationDateime
+  print(overTime, ' sum')
+  returnTime = overTime.strftime("%I:%M %p")[1:] if overTime.strftime(
+    "%I:%M %p")[0] == '0' else overTime.strftime("%I:%M %p")
+  daysPassed = int(overTime.strftime("%d")) - 1
+  print(daysPassed)
+  strDaysPassed = f' ({daysPassed} days later)' if (
+    daysPassed) > 1 else ' (next day)' if (daysPassed) == 1 else ''
+  if weekDay:
+    print(daysPassed % 7)
+    weekList = [
+      'monday', 'tuesday', 'wednesday', 'thurday', 'friday', 'saturday',
+      'sunday'
+    ]
+    weekDayIndex = weekList.index(weekDay)
+    print((daysPassed % 7 + weekDayIndex) % 7)
+    newDay = f', {str(weekList[(daysPassed%7+weekDayIndex)%7]).capitalize()}'
+    print(newDay)
 
-add_time("11:30 AM", "2:32", "Monday")
-# Returns: 2:02 PM, Monday
-
-add_time("11:43 AM", "00:20")
-# Returns: 12:03 PM
-
-add_time("10:10 PM", "3:30")
-# Returns: 1:40 AM (next day)
-
-add_time("11:43 PM", "24:20", "tueSday")
-# Returns: 12:03 AM, Thursday (2 days later)
-
-add_time("6:30 PM", "205:12")
-# Returns: 7:42 AM (9 days later)
-Do not import any Python libraries. Assume that the start times are valid times. The minutes in the duration time will be a whole number less than 60, but the hour can be any whole number.
-
-Development
-Write your code in time_calculator.py. For development, you can use main.py to test your time_calculator() function. Click the "run" button and main.py will run.
-
-Testing
-The unit tests for this project are in test_module.py. We imported the tests from test_module.py to main.py for your convenience. The tests will run automatically whenever you hit the "run" button.
-
+  print('-' * 20)
+  return f"{returnTime}{strDaysPassed}" if not weekDay else f"{returnTime}{newDay}{strDaysPassed}"
